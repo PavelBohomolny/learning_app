ef9cc2a4a99b08aed3a99ed333a841f740d3c701
NexusUA <pbogomolny93@gmal.com>
2022-08-03 11:32:37 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Pavel Bohomolnyi on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
